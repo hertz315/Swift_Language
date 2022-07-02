3ccab9d23ca540e08cb7bdc725e27c48f6e6d6f8
Boombaya <hertz315@gmail.com>
2022-07-02 21:58:39 +0900

    Function update

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -163,10 +163,25 @@ func stepBackward(_ input: Int) -> Int {
     return input - 1
 }
 
+//func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+//    return backward ? stepBackward : stepForward
+//}
+
+//var currentValue = 3
+//let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero now refers to the stepBackward() function
+
 func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
     return backward ? stepBackward : stepForward
 }
 
-var currentValue = 3
+var currentValue = -4
 let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
-// moveNearerToZero now refers to the stepBackward() function
+
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
