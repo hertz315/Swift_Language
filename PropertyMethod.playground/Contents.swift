afaca220c1b25fa6aae7890ca9df81c485cbe862
Boombaya <hertz315@gmail.com>
2022-07-09 21:11:25 +0900

    저장속성 update

--- PropertyMethod.playground/Contents.swift
+++ PropertyMethod.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//:> 저장 속성(Stored Properties)
+
+struct Bird {
+    var name: String
+    var weight: Double
+    
+    init(name: String, weight: Double) {
+        self.name = name
+        self.weight = weight
+    }
+    
+    func fly() {
+        print("날아갑니다.")
+    }
+}
+
+var aBird = Bird(name: "독수리", weight: 3.3)
+aBird.name // "독수리"
+aBird.weight = 3.7
+aBird.weight // 3.7
+
+
