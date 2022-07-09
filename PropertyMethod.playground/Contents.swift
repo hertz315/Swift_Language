64218c9fa0f5330916033bd60f90adcda32bd567
Boombaya <hertz315@gmail.com>
2022-07-09 23:51:15 +0900

    계산속성 update

--- PropertyMethod.playground/Contents.swift
+++ PropertyMethod.playground/Contents.swift
@@ -64,3 +64,65 @@ class AView {
 var a: AView = AView(num: 3)
 a.b // 30 // b 지연 저장 속성에 접근한느 순간 메모리에 공간이 생기고 저장 속성 a의 값을 이용하여 값을 저장함
 
+//:> 계산 속성(Computed Properties)
+
+class Person {
+    var birth: Int
+    var thisYear: Int
+    
+    var age: Int {
+        get {
+            return (thisYear - birth)+1
+        } set(age) {
+            self.birth = (thisYear - age)+1
+        }
+    }
+    
+    init(birth: Int, thisYear: Int) {
+        self.birth = birth
+        self.thisYear = thisYear
+    }
+}
+
+var p: Person = Person(birth: 1996, thisYear: 2022)
+p.age // 27
+p.birth // 1996
+p.age = 27
+p.birth // 1996
+
+/*:
+ ---
+ */
+
+// (BMI) 계산
+
+class PersonUpdate {
+    // 저장 속성
+    var name: String
+    var height: Double
+    var weight: Double
+    // 175.0 //30625
+    var bmi: Double {
+        get{
+            // get == 값을 얻는 다는 의미
+            let result = weight / (height * height) * 10000
+            return result
+        } set {
+            weight = newValue * height * height / 10000
+        }
+        
+    }
+    
+    init(name: String, height: Double, weight: Double) {
+        self.name = name
+        self.height = height
+        self.weight = weight
+    }
+    
+}
+
+var pUpdate = PersonUpdate(name: "Tom", height: 175.0, weight: 75)
+pUpdate.bmi  // 24.489
+pUpdate.weight // 75
+pUpdate.bmi = 20
+pUpdate.weight // 61.25
