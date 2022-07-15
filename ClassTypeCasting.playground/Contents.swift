d34e52dd2a9d61a51d8369a818880a162387ffe4
Boombaya <hertz315@gmail.com>
2022-07-15 21:34:49 +0900

    Any타입 update

--- ClassTypeCasting.playground/Contents.swift
+++ ClassTypeCasting.playground/Contents.swift
@@ -239,3 +239,101 @@ ppeople[1].walk()
 // 학생이 걷는다.
 ppeople[2].walk()
 // 대학생이 걷는다.
+
+//:> Any와 AnyObject 타입
+
+/**===================================================================
+ - 스위프트에서 제공하는 불특정한 타입을 다룰 수 있는 타입을 제공
+ 
+ 1) Any 타입
+ - 기본 타입(Int, String, Bool, ...) 등 포함, 커스텀 클래스, 구조체, 열거형, 함수타입
+   까지도 포함해서 어떤 타입의 인스턴스도 표현할 수 있는 타입 (옵셔널타입도 포함)
+ 
+ 2) AnyObject 타입
+ - 어떤 클래스 타입의 인스턴스도 담을수 있는 타입
+======================================================================**/
+
+var some: Any = "Swift"
+// some.count   // error
+(some as? String)!.count    // 5
+some = 10
+some = 3.2
+
+// Any 타입 사용시 단점
+// 저장된 타입의 메모리 구조를 알 수 없기 때문에 ⭐️항상 타입캐스팅해서 사용해야한다
+
+class anyPerson {
+    var name = "이름"
+    var age = 10
+}
+
+class anySuperman {
+    var name = "이름"
+    var weight = 100
+}
+
+let anyperson = anyPerson()
+
+// Any타입의 장점: 모든 타입을 담을 수 있는 배열을 생성 가능
+let array: [Any] = [5, "안녕", 3.5, anyPerson(), anySuperman(), {(name: String) in return name}]
+
+(array[1] as! String).count // 2
+
+// 2) AnyObject타입 =======================================
+
+
+let objArray: [AnyObject] = [anyPerson(), anyperson, anySuperman(), NSString()]
+
+//objArray[0].name
+(objArray[0] as! anyPerson).name    // "이름"
+
+
+
+// 타입캐스팅 + 분기처리
+// let array: [Any] = [5, "안녕", 3.5, anyPerson(), anySuperman(), {(name: String) in return name}]
+
+for (index, item) in array.enumerated() {
+    switch item {
+    case is Int:    // item is Int == Bool
+        print("Index - \(index): \(item)정수입니다.")
+    case let num as Double: // let num = item as Double
+        print("Index - \(index): 소수 \(num)입니다.")
+    case is String: // item is String == Bool
+        print("Index - \(index): 문자열입니다.")
+    case let person as anyPerson:                    // let person = item as anyPerson
+        print("Index - \(index): 사람입니다.")
+        print("이름은 \(person.name)입니다.")
+        print("나이는 \(person.age)입니다.")
+    case is (String) -> String:                   // item is (String) -> String
+        print("Index - \(index): 클로저 타입입니다.")
+    default:
+        print("Index - \(index): 그 이외의 타입입니다.")
+    }
+}
+// Index - 0: 5정수입니다.
+// Index - 1: 문자열입니다.
+// Index - 2: 소수 3.5입니다.
+// Index - 3: 사람입니다.
+// 이름은 이름입니다.
+// 나이는 10입니다.
+// Index - 4: 그 이외의 타입입니다.
+// Index - 5: 클로저 타입입니다.
+
+//:> 옵셔널값의 Any 변환
+
+/**==================================================================
+- 의도적인 옵셔널값의 사용
+- Any는 모든 타입을 포함하므로, 의도적인 옵셔널값을 사용하려면 Any타입으로 변환하면
+  컴파일러가 알려주는 경고를 없앨 수 있음
+ 
+- 왜?
+- 옵셔널값은 임시적인 값일 뿐이므로, 일반적으로 개발자들은 옵셔널 바인딩을 해서
+  언래핑해서 안의 값을 사용해야함 ===> 그래서 경고를 통해 알려줌
+  (Any로 변환하겠다는 것은 그 자체를 사용하겠다는 의미임 ===> 경고 없음)
+==================================================================**/
+
+
+
+let optionalNumber: Int? = 3
+print(optionalNumber)          // 경고
+print(optionalNumber as Any)   // 경고 없음
