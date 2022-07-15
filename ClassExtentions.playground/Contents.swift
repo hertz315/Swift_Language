31fc93de2f7b747af6cfcc3d6c8e739458259d8e
Boombaya <hertz315@gmail.com>
2022-07-15 22:51:19 +0900

    확장 멤버 update

--- ClassExtentions.playground/Contents.swift
+++ ClassExtentions.playground/Contents.swift
@@ -115,3 +115,45 @@ extension Int {
  
  - 프로토콜에 대한 확장도 가능(자세한 내용은 프로토콜에서 다룰 예정)
  ============================================================================================**/
+
+//:> 멤버의 확장(계산 속성)
+
+// 타입 계산 속성
+extension Double {
+    static var zero: Double {
+        return 0.0
+    }
+}
+
+Double.zero
+// 0.0
+
+// 인스턴스 계산 속성
+
+// 인스턴스의 (읽기전용) 계산 속성을 기존의 애플이 만든 Double 타입에 추가 하는 예제
+// 자신의 단위를 입력하여 ==> 미터 기준으로 바꾸는 예제
+
+extension Double {
+    var km: Double {
+        return self * 1000.0
+    }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+let oneInch = 25.4.mm
+print("1인치는 \(oneInch) 미터")
+// 1인치는 0.0254 미터
+
+let threeFeet = 3.ft
+print("3피트는 \(threeFeet) 미터")
+// 3피트는 0.914399970739201 미터
+
+let aMarathon = 42.km + 195.m
+print("마라톤은 \(aMarathon) 미터의 길이임")
+// 마라톤은 42195.0 미터의 길이임
+
+
+
