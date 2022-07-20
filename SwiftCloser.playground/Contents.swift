c141a06c5b0ddea0671cd9553eded64afdc6de10
Boombaya <hertz315@gmail.com>
2022-07-20 11:12:33 +0900

    클로저 사용 update

--- SwiftCloser.playground/Contents.swift
+++ SwiftCloser.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import os
 
 //:> 클로저 - 이름이 없는(익명) 함수
 
@@ -75,4 +76,54 @@ let aClosure3 = {print("This is a closure.")}
 // 파라미터의 타입의 생략도 대부분 가능하다
 let closure4 = { str in
     return str + "!"
+}
+
+//:> 클로저의 사용
+// 1) (클로저를 파라미터로 받는 함수)정의
+func closureParamFuction(closure: () -> ()) {
+    print("프린트 시작")
+    closure()
+}
+
+// 파라미터로 사용할 함수/클로저를 정의
+// 함수 정의
+func printSwiftFunction() {
+    print("프린트 종료")
+}
+
+// 클로저를 정의
+let printSwift = {() -> () in
+    print("프린트 종료")
+}
+
+// 함수를 파라미터로 넣으면서 실행(그동안에 배운 형태로 실행)
+closureParamFuction(closure: printSwiftFunction)
+
+closureParamFuction(closure: printSwift)
+
+// 2) 함수를 실행할때 클로저 형태로 전달(클로저를 사용하는 이유)
+closureParamFuction(closure: {() -> () in
+    // 본래 정의된 함수를 실행시키면서, 클로저를 사후적으로 정의 가능 (활용도가 늘어남)
+    print("프린트 종료")
+})
+
+//:> 클로저를 왜 사용하는가 2
+// 1) (클로저를 파라미터로 받는 함수)정의
+func closureCaseFunction(a: Int, b: Int, closure: (Int) -> Void) {
+    let c = a + b
+    closure(c)
+}
+
+// 2) 함수를 실행할 때 (클로저 형태로 전달)
+closureCaseFunction(a: 5, b: 5, closure: {(c) in
+    print("이제 출력할께요: \(c)")
+})
+
+closureCaseFunction(a: 10, b: 10, closure: {(number) in
+    print("출력할께요 \(number)")
+})
+
+closureCaseFunction(a: 20, b: 20) {(n) in
+    print("출력")
+    print("값: \(n)")
 }
